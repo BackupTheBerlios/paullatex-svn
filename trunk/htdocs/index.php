6cb0ac38262cc1b195b1cedc3a39b98890af9346
epaul <epaul@a186d9f6-6c64-0410-8841-9b89bca81532>
2011-10-08 19:07:03 +0000

    Umzug zu GitHub.

--- trunk/htdocs/index.php
+++ trunk/htdocs/index.php
@@ -20,10 +20,13 @@ require_once ("./neu-liste.php.inc");
   <body>
 
 	<div class="disclaimer">
-	<h3>Werbung</h3>
-	<p>
-<a href="http://developer.berlios.de" title="BerliOS Developer"> <img src="http://developer.berlios.de/bslogo.php?group_id=6747" width="124px" height="32px" border="0" alt=""><br/>Berlios Developer</a> stellt uns Subversion, Webseite und Entwicklungstools zur Verfügung – danke!
-	</p>
+	<h3>Umzug</h3>
+<p>Nachdem BerliOS
+<a href='http://developer.berlios.de/forum/forum.php?forum_id=37450'>
+angekündigt hat, zum Jahresende die Pforten zu schließen</a>, sind
+<a href='http://epaul.github.com/paullatex/'>Paul's LaTeX-Pakete jetzt
+bei Github zu finden</a>.
+</p>
 	</div>
 
 <div class='inhalt'>
